63598d6164aab5ce0389273c92292aaaf621056e
pavel_k <pavel_k@mail.com>
2025-01-19 15:03:35 +0300

    intset

---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+type IntSet struct {
+	words []uint64
+}
+
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/64, uint(x%64)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
+func (s *IntSet) Remove(x int) {
+	word, bit := x/64, uint(x%64)
+	if word < len(s.words) && s.words[word]&(1<<bit) != 0 {
+		s.words[word] &= ^(1 << bit)
+	}
+}
+func (s *IntSet) Add(x int) {
+	word, bit := x/64, uint(x%64)
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] |= 1 << bit
+}
+func (s *IntSet) UnionWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] |= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+func (s *IntSet) Clear() {
+	s.words = s.words[:0]
+}
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", 64*i+j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
+func (s *IntSet) Len() int {
+	var len int
+	for _, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				len++
+			}
+		}
+	}
+	return len
+}
+func (s *IntSet) IntersectWith(t *IntSet) {
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				num := 64*i + j
+				if !t.Has(num) {
+					s.Remove(num)
+				}
+			}
+		}
+	}
+}
+func (s *IntSet) Copy() IntSet {
+	var x IntSet
+	cpy := make([]uint64, len(s.words))
+	copy(cpy, s.words)
+	x.words = cpy
+	return x
+}
+func (s *IntSet) AddAll(x ...int) {
+	for _, v := range x {
+		s.Add(v)
+	}
+}
+
+func main() {
+}
